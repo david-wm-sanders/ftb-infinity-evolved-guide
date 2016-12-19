a1c582adf5f867767b2239cde90c185c488c0069
David Sanders <david.wm.sanders@gmail.com>
2016-12-19 04:26:32 +0000

    Minor refactors

--- scripts/markdown_flower_mutation.py
+++ scripts/markdown_flower_mutation.py
@@ -5,34 +5,27 @@ from pathlib import Path
 ip = Path("./data/flower_mutation.csv")
 op = Path("./data/flower_mutation.mdi")
 
-# Read the input CSV file into a dictionary
-flowers_d = {}
+# Read and process the relevant data from the input CSV file into a list of flowers
+flowers = []
 with ip.open() as csv_file:
     reader = DictReader(csv_file)
     for row in reader:
-        flowers_d[row["UID"]] = {"name": row["Name"], "a0": row["Allele0"],
-                                    "a1": row["Allele1"], "chance": row["baseChance"]}
-
-# Mangle the dictionary into a list
-flowers = []
-for uid, flower in flowers_d.items():
-    f = (flower["name"],
-         flower["a0"].split(".")[3].capitalize(),
-         flower["a1"].split(".")[3].capitalize(),
-         "{:G}%".format(float(flower["chance"])))
-    flowers.append(f)
+        f = (row["Name"],
+             row["Allele0"].split(".")[3].capitalize(),
+             row["Allele1"].split(".")[3].capitalize(),
+             "{:G}%".format(float(row["baseChance"])))
+        flowers.append(f)
 
 # Sort the flowers by name
 flowers.sort(key=itemgetter(0))
 
 # Create a Markdown table from the list
-md_table = ["| Flower Z | Flower X | Flower Y | Chance |",
-            "| :-- | :-- | :-- | --: |"]
+md_table = ["| Flower Z | Flower X | Flower Y | Chance |\n",
+            "| :-- | :-- | :-- | --: |\n"]
 for f in flowers:
-    md_table.append("| {0[0]} | {0[1]} | {0[2]} | {0[3]} |".format(f))
+    md_table.append("| {0[0]} | {0[1]} | {0[2]} | {0[3]} |\n".format(f))
 
 # Write the Markdown table to a file
 with op.open("w") as mdi_file:
     for row in md_table:
         mdi_file.write(row)
-        mdi_file.write("\n")
